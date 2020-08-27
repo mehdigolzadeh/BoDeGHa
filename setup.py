2b5dfe3378a88be9f7940292f536e4974bd424f1
MhDG7 <mehdi.golzadeh@umons.ac.be>
2020-08-27 15:51:37 +0200

    bug fixes in setup.py

--- setup.py
+++ setup.py
@@ -36,58 +36,30 @@ __requirement__ = [
         'python-levenshtein >= 0.12.0',
 ]
 
-datadir = os.path.join('share','data')
-datafiles = [(d, [os.path.join(d,f) for f in files])
-    for d, folders, files in os.walk(datadir)]
-
-
 setup(
     name=__package__,
 
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # http://packaging.python.org/en/latest/tutorial.html#version
     version=__version__,
 
     description= __description__,
     long_description=__long_description__,
 
-    # The project's main homepage.
     url=__url__,
 
-    # Maintainer details (this is a backport)
     maintainer=__maintainer__,
     maintainer_email=__email__,
 
-    # Choose your license
     license=__licence__,
 
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=__classifiers__,
 
-    # What does your project relate to?
     keywords='github bot account comment',
 
-    # List run-time dependencies here.  These will be installed by pip when your
-    # project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
     install_requires = __requirement__,
 
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    # package_data={
-    #     '': ['*.rst']
-    # },
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages.
-    # see http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    ## data_files=[('my_data', ['data/data_file'])],
-
-    data_files =[('bodega_data', ['model.pkl'])],
+    package_data={
+        '': ['*.pkl']
+    },
 
     entry_points={
         'console_scripts': [
