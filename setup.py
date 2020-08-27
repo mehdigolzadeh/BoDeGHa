86441b6b503c14dc57c7affc43162c956dfcbf26
MhDG7 <mehdi.golzadeh@umons.ac.be>
2020-08-27 17:57:27 +0200

    updates

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
 
     install_requires = __requirement__,
 
-    package_data={'package':['bodegamodel/model.json']},
+    include_package_data=True,
 
     entry_points={
         'console_scripts': [
